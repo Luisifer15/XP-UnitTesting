2e35788462e59bced79dd301a1079e0dcb236d34
Luis Rimando <alfonsorimando15@gmail.com>
2024-04-22 18:12:15 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XP_UnitTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
