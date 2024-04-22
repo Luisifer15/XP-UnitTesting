9d366e83db9573b9ffd780ebb5f78e2e327b4918
Jason Gorospe <9.pjgorospe@gmail.com>
2024-04-22 19:59:26 +0800

    BlogsRepositoryTest update (some still not working)

--- UnitTesting/BlogsRepositoryTest.cs
+++ UnitTesting/BlogsRepositoryTest.cs
@@ -58,5 +58,80 @@ namespace XP_UnitTesting.UnitTesting
             }
         }
 
+        [Test]
+        public void AddBlog()
+        {
+            //ARRANGE
+            var author = new Author
+            {
+                Email = "test@example.com",
+                Name = "Test Author",
+                DateCreated = DateTime.Now
+            };
+
+            var blog = new BlogPost
+            {
+                Title = "Test Blog Post",
+                BlogContent = "This is a test blog post content.",
+                Slug = "test-blog-post",
+                AuthorId = author.Id
+            };
+
+            using (var entities = new BlogsContext(_options!))
+            {
+                var authorRepo = new AuthorsRepository(entities);
+                authorRepo.AddAuthor(author);
+
+                var blogRepo = new BlogsRepository(entities);
+                // Act
+                blogRepo.AddBlog(blog);
+                entities.SaveChanges();
+
+                // Assert
+                var addedBlog = entities.BlogPosts.FirstOrDefault(b => b.Id == blog.Id);
+                Assert.That(blog.Title, Is.EqualTo(addedBlog.Title));
+                Assert.That(blog.BlogContent, Is.EqualTo(addedBlog.BlogContent));
+                Assert.That(blog.Id, Is.EqualTo(addedBlog.Id));
+                Assert.That(blog.Slug, Is.EqualTo(addedBlog.Slug));
+            }
+        }
+
+        [Test]
+        public void RemoveBlog()
+        {
+            // Arrange
+            var author = new Author
+            {
+                Email = "test@example.com",
+                Name = "Test Author",
+                DateCreated = DateTime.Now
+            };
+
+            var blog = new BlogPost
+            {
+                Title = "Test Blog Post",
+                BlogContent = "This is a test blog post content.",
+                Slug = "test-blog-post",
+                AuthorId = author.Id
+            };
+
+            using (var entities = new BlogsContext(_options!))
+            {
+                var authorRepo = new AuthorsRepository(entities);
+                authorRepo.AddAuthor(author);
+                var blogRepo = new BlogsRepository(entities);
+                blogRepo.AddBlog(blog);
+
+                var existingBlog = entities.BlogPosts.FirstOrDefault(b => b.Id == blog.Id);
+                Assert.That(existingBlog, Is.Not.Null);
+                //ACT
+                blogRepo.RemoveBlog(blog.Id);
+                var blogList = blogRepo.GetBlogs();
+
+                //ASSERT
+                var removedBlog = entities.BlogPosts.FirstOrDefault(b => b.Id == blog.Id);
+                Assert.That(removedBlog, Is.Null);
+            }
+        }
     }
 }
