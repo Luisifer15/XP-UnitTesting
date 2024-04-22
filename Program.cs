a7f3f3cb3905ce4bdad37c851acde8a624d93cc0
Luis Rimando <alfonsorimando15@gmail.com>
2024-04-22 18:19:41 +0800

    NuGet Packages + EntityFramework parameters

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using XP_UnitTesting.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BlogsContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection"));
+});
 
 var app = builder.Build();
 
